2e97184b68a371177ba5773339ea7f6206fef527
codebseraph <felipejean1@gmail.com>
2017-01-04 23:43:38 -0200

    Initial commit

---
+++ phoneapps.mk
@@ -0,0 +1,37 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := \
+	BasicDreams \
+	Bluetooth \
+	BluetoothExt \
+	CertInstaller \
+	FM2 \
+	KeyChain \
+	LatinIME \
+	LiveWallpapers \
+	NfcNci \
+	PacProcessor \
+	PicoTts \
+	PrintSpooler \
+	Stk \
+	telresources \
+	TimeService \
+	WAPPushManager \
+	webview 
+
+private-phone-priv-apps := \
+	AudioFX \
+	BackupRestoreConfirmation \
+	CarrierConfig \
+	CellBroadcastReceiver \
+	CMSettingsProvider \
+	ExternalStorageProvider \
+	FusedLocation \
+	InputDevices \
+	OneTimeinitializer \
+	ProxyHandler \
+	Screencast \
+	SharedStorageBackup \
+	Shell \
+	StatementService 
